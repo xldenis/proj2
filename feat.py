4b52ed724738d4e02e47e18448c4d6f4e0565b1f
Xavier Denis <xldenis@gmail.com>
2014-10-10 16:12:07 -0400

    Filter stop words and rare words

--- feat.py
+++ feat.py
@@ -3,8 +3,10 @@ from collections import defaultdict
 from math import log
 import csv
 
+STOP_WORDS = ["a", "also", "an", "and", "are", "as", "at", "be", "by", "can", "for", "from", "has", "in", "is", "it", "of", "on", "our", "show", "such", "that", "the", "these", "this", "to", "using", "we", "which", "with"]
+
 def strip(abs):
-  return ' '.join(re.split('\W+', abs))
+  return ' '.join([w.lower() for w in re.findall('\w+', abs) if w not in STOP_WORDS])
 
 def tfidf(corpus):
   tf = {}
@@ -20,7 +22,9 @@ def tfidf(corpus):
   for doc in corpus:
     tfidf[doc['id']] = defaultdict(lambda: 0)
     for word in tf[doc['id']]:
-      tfidf[doc['id']][word] = tf[doc['id']][word] * log(len(corpus) / float(1 + counts[word]))
+      if counts[word] >= 3:
+        tfidf[doc['id']][word] = tf[doc['id']][word] * log(len(corpus) / float(1 + counts[word]))
+
   return tfidf
 
 def load_training():
@@ -51,5 +55,4 @@ def main():
   train = load_training()
   print "TF-IDF test"
   print tfidf(train)
-
 if  __name__ =='__main__':main()
